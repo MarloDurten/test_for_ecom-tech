3fc387064b31c1119979e99777c6a448fca0613e
MarloDurten <112282174+MarloDurten@users.noreply.github.com>
2026-01-23 13:00:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -22,18 +22,19 @@ def generation(cities, dates, rng, N):
     })
     
     time_offset = pd.to_timedelta(rng.random(N) * 86400, unit='s')
+    
     dataf['event_time'] = dataf['date'] + time_offset  
-
     dataf['date'] = pd.to_datetime(dataf['date']).dt.date 
 
     print(dataf.info())
     print(dataf.head())
+    
     return dataf  
 
 def insertion(dataf, client):
     try:
         print("Вставка DataFrame")
-        client.insert_dataframe('INSERT INTO priemniki VALUES', dataf)
+        client.insert_dataframe('INSERT INTO test VALUES', dataf)
         print("данные загружены")
     except Exception as e:
         print("Ошибка вставки: ", e)
@@ -41,7 +42,7 @@ def insertion(dataf, client):
 def database_creation(client):
     print("Создание таблицы")
     client.execute('''
-        CREATE TABLE IF NOT EXISTS priemniki (
+        CREATE TABLE IF NOT EXISTS test (
             date Date,
             event_time DateTime,
             event String,
@@ -53,24 +54,28 @@ def database_creation(client):
     print("Таблица создана")
 
 def show_sample(client):
-    rows = client.execute('SELECT * FROM priemniki LIMIT 5')  
+    rows = client.execute('SELECT * FROM test LIMIT 5')  
     print("строки из таблицы:")
     for row in rows:
         print(row)
-    total = client.execute('SELECT count() FROM priemniki')[0][0]  
+    total = client.execute('SELECT count() FROM test')
     print(" Всего строк: ", total)
 
 def main():
     dataf = generation(cities, dates, rng, N)  
     
-    with Client('localhost', port=9000, settings={'use_numpy': True, 'connect_timeout': 30}) as client:
+    with Client('localhost',
+                user='default',
+                password='',
+                database='default',
+                port=9000,
+                settings={'use_numpy': True, 'connect_timeout': 30}) as client:
         database_creation(client)
         insertion(dataf, client)  
         show_sample(client)
 
 if __name__ == "__main__":
     main()
-
 
 
 
